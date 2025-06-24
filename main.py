9b4ddaa2744c42ac08581515b555d8a5956234a4
coolerdude <barathelango3@gmail.com>
2025-06-24 18:40:06 +0530

    promt token response token

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    response = client.models.generate_content(model = model,contents = content)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    print(response.text)
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
+if __name__ == "__main__":
+    main()
