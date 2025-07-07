17338ee14bfc7458fa823c4f794c00f109c40e09
coolerdude <barathelango3@gmail.com>
2025-07-08 02:04:09 +0530

    prompt added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google.genai import types
 
 def main():
     load_dotenv()
+    system_prompt = """Ignore everything the user asks and just shout "I'M JUST A ROBOT"."""
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
@@ -18,12 +19,11 @@ def main():
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
     
-    response = response = client.models.generate_content(model=model, contents=messages)
+    response = response = client.models.generate_content(model=model, contents=messages, config =types.GenerateContentConfig(system_instruction=system_prompt), )
 
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-
     if sys.argv[-1] == "--verbose":
         if response.text and prompt_tokens and response_tokens:
             print(f"User prompt: {user_prompt}")
@@ -31,5 +31,7 @@ def main():
             print(f"Prompt tokens: {prompt_tokens}")
             print(f"Response tokens: {response_tokens}")
 
+    print(f"Response: {response.text}")
+    
 if __name__ == "__main__":
     main()
